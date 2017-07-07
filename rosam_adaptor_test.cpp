78b251d176da3cdc5d25e01ab8540f6f1f269881
Ignacio Vizzo <ivizzo@irobot.com>
2017-07-07 01:13:13 -0700

    Make the polygon_test more generic

--- rosam_adaptor_test.cpp
+++ rosam_adaptor_test.cpp
@@ -59,60 +59,40 @@ void test_point() {
 
 //first lets turn our polygon usage code into a generic
 //function parameterized by polygon type
-template<typename Polygon>
-  void
-  test_polygon ()
-  {
-    //lets construct a 10x10 rectangle shaped polygon
-    typedef typename gtl::polygon_traits<Polygon>::point_type Point;
-    gtl::rectangle_data<double> rect;
+template <typename Polygon, typename coordinate_type>
+void test_polygon()
+{
+  //lets construct a 10x10 rectangle shaped polygon
+  typedef typename gtl::polygon_traits<Polygon>::point_type Point;
+  gtl::rectangle_data<coordinate_type> rect;
 
+  Point pts[] =
+      {gtl::construct<Point>(0, 0), gtl::construct<Point>(10, 0),
+       gtl::construct<Point>(10, 10), gtl::construct<Point>(0, 10)};
+  Polygon poly;
+  gtl::set_points(poly, pts, pts + 4);
 
-    Point pts[] =
-	  { gtl::construct<Point> (0.0, 0.0), gtl::construct<Point> (10.0, 0.0),
-	      gtl::construct<Point> (10.0, 10.0), gtl::construct<Point> (0.0,
-									 10.0) };
-    Polygon poly;
-    gtl::set_points (poly, pts, pts + 4);
+  //now lets see what we can do with this polygon
+  assert(gtl::area(poly) == 100.0f);
+  assert(gtl::contains(poly, gtl::construct<Point>(5, 5)));
+  assert(!gtl::contains(poly, gtl::construct<Point>(15, 5)));
+  assert(gtl::extents(rect, poly));     //get bounding box of poly
+  assert(gtl::equivalence(rect, poly)); //hey, that's slick
+  assert(gtl::winding(poly) == gtl::COUNTERCLOCKWISE);
+  assert(gtl::perimeter(poly) == 40.0f);
 
-    std::cout << "Poly = [" << std::endl;
+  //add 5 to all coords of poly
+  gtl::convolve(poly, gtl::construct<Point>(5.0, 5.0));
+  gtl::set_points(rect, gtl::point_data<coordinate_type>(5.0, 5.0),
+                  gtl::point_data<coordinate_type>(15.0, 15.0));
+  assert(gtl::equivalence(poly, rect));
 
-    for (int i = 0; i < poly.size(); ++i) {
-	std::cout << poly[i].x() << "," <<poly[i].y() << ";" <<std::endl;
-    }
-    std::cout << "];" <<std::endl;
-
-
-    //now lets see what we can do with this polygon
-    assert(gtl::area (poly) == 100.0f);
-    assert(gtl::contains (poly, gtl::construct<Point> (5, 5)));
-    assert(!gtl::contains (poly, gtl::construct<Point> (15, 5)));
-    assert(gtl::extents (rect, poly)); //get bounding box of poly
-    assert(gtl::equivalence (rect, poly)); //hey, that's slick
-    assert(gtl::winding (poly) == gtl::COUNTERCLOCKWISE);
-    assert(gtl::perimeter (poly) == 40.0f);
-
-    //add 5 to all coords of poly
-    gtl::convolve (poly, gtl::construct<Point> (5.0, 5.0));
-
-    std::cout << "Poly after convolve= [" << std::endl;
-
-    for (int i = 0; i < poly.size(); ++i) {
-	std::cout << poly[i].x() << "," <<poly[i].y() << ";" <<std::endl;
-    }
-    std::cout << "];" <<std::endl;
-
-    gtl::set_points (rect, gtl::point_data<double> (5.0, 5.0),
-		     gtl::point_data<double> (15.0, 15.0));
-    assert(gtl::equivalence (poly, rect));
-
-
-    //multiply all coords of poly by 2
-    gtl::scale_up (poly, 2.0);
-    gtl::set_points (rect, gtl::point_data<double> (10.0, 10.0),
-		     gtl::point_data<double> (30.0, 30.0));
-    assert(gtl::equivalence (poly, rect));
-  }
+  //multiply all coords of poly by 2
+  gtl::scale_up(poly, 2.0);
+  gtl::set_points(rect, gtl::point_data<coordinate_type>(10.0, 10.0),
+                  gtl::point_data<coordinate_type>(30.0, 30.0));
+  assert(gtl::equivalence(poly, rect));
+}
 
 //once again we make our usage of the library generic
 //and parameterize it on the polygon set type
