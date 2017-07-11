a09ee5e82dc4d7f6a393ac79f133f74837711cd4
Ignacio Vizzo <ivizzo@irobot.com>
2017-07-10 18:44:29 -0700

    Remove gcc 7 dependencies

--- build/subdir.mk
+++ build/subdir.mk
@@ -17,7 +17,7 @@ CPP_DEPS += \
 %.o: ../%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++-7 -std=c++0x -Wfatal-errors -O0 -g3 -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -std=c++0x -Wfatal-errors -O0 -g3 -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
